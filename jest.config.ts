132e6b6a533b02fd7a9af4e139a0152a33d9b422
Paul Yoon <143664110+gi5229@users.noreply.github.com>
2024-11-19 23:21:36 -0500

    updated package.json file and added new .ts file
    Updated .json file to integrate jest testing and installed dependencies, also added a new .ts file to update the configurations for the unit testing framework.

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'jest-expo',
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  clearMocks: true,
+  resetMocks: true,
+  testTimeout: 10000
+};
+
+export default config;
