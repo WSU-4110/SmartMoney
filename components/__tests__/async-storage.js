d017e9404372e0854194bc43d8f16d4a0bbd9673
Paul Yoon <143664110+gi5229@users.noreply.github.com>
2024-12-05 22:41:27 -0500

    grifs dependencies
    griffins unit test dependencies

---
+++ components/__tests__/async-storage.js
@@ -0,0 +1,11 @@
+export default {
+    setItem: jest.fn(() => Promise.resolve()),
+    getItem: jest.fn(() => Promise.resolve(null)),
+    removeItem: jest.fn(() => Promise.resolve()),
+    clear: jest.fn(() => Promise.resolve()),
+    getAllKeys: jest.fn(() => Promise.resolve([])),
+    multiGet: jest.fn(() => Promise.resolve([])),
+    multiSet: jest.fn(() => Promise.resolve()),
+    multiRemove: jest.fn(() => Promise.resolve()),
+    mergeItem: jest.fn(() => Promise.resolve()),
+  };
